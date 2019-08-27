0187f2b4e975ff9d1ca8b15be8c294bb41723ac7
XIX <xiuming.x@outlook.com>
2019-08-26 18:05:54 -0700

    ControlledComponent Done

--- ControlledComponent/src/App.js
+++ ControlledComponent/src/App.js
@@ -3,6 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state={
+    something: ''
+  }
+  handleSomething = (val) =>{
+    this.setState({something: val});
+
+  }
   render() {
     return (
       <div className="App">
@@ -11,9 +18,13 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <div className="container">
-          <input type="text" placeholder="Say Something" />
+          <input 
+             type="text" 
+             placeholder="Say Something" 
+             value={this.state.something}
+             onChange={(event) => this.handleSomething(event.target.value)}/>
           <p className="echo">Echo:</p>
-          <p>This should mirror the text you typed into the input field.</p>
+          <p>{this.state.something}</p>
         </div>
       </div>
     );
